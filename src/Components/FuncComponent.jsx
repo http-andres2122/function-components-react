f501b5c1aaf15f611cffbb1ddf49f2d4c09e5e7b
Andres B <19674617+http-andres2122@users.noreply.github.com>
2023-05-22 15:56:09 -0500

    finaliza modulo 1

--- src/Components/FuncComponent.jsx
+++ src/Components/FuncComponent.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 function FuncComponent () {
-    return <div>Componente funcional </div>
+    const [value, setValue]=useState(0)
+    
+    return (
+        <div>
+            Componente funcional 
+            <p>
+                <button
+                    onClick={() => setValue(value - 1)}
+                >-</button> {value}
+                <button
+                    onClick={() => setValue(value + 1)}
+                >+</button>
+            </p>    
+        </div>
+    ) 
+        
 };
 
 export default FuncComponent;
