514c2ca9ecc9b4191fc6d02e3ce7731b332010c5
Andres B <19674617+http-andres2122@users.noreply.github.com>
2023-05-22 22:43:49 -0500

    se acaba modulo-2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import FormSignUp from './Components/FormSignUp';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 
 function App() {
   return (
-    <>
-      <h1>Formulario Registro</h1>
+    <Container component="section" maxWidth="sm">
+      <Typography variant='h3' align="center" component={"h3"}>Formulario Registro</Typography>
       <FormSignUp/>
-    </>
+    </Container>
 
   );
 }
--- src/Components/FormSignUp.jsx
+++ src/Components/FormSignUp.jsx
@@ -1,17 +1,40 @@
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Switch from '@mui/material/Switch';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
 
 function FormSignUp () {
     return <form>
-        <label>Nombre</label>
-        <input type="text" />
-        <label>Apellidos</label>
-        <input type="text" />
-        <label>Correo Electronico</label>
-        <input type="email" />
-        <label>Promociones</label>
-        <input type="checkbox" />
-        <label>Novedades</label>
-        <input type="checkbox" />
+        <TextField 
+            id="name" 
+            label="Nombre" 
+            variant="filled" 
+            type="text" 
+            fullWidth
+            margin='normal'
+        />
+        <TextField 
+            id="lastname" 
+            label="Apellidos" 
+            variant="filled" 
+            type="text"
+            fullWidth
+            margin='normal'
+        />
+        <TextField 
+            id="email" 
+            label="Correo electronico" 
+            variant="filled" 
+            type="email"
+            fullWidth
+            margin='normal'
+        />
+        <FormGroup>
+            <FormControlLabel control={ <Switch defaultChecked /> } label="Promociones" />
+            <FormControlLabel control={ <Switch defaultChecked /> } label="Novedades" />
+        </FormGroup>
         <Button variant='contained'>Registrarse</Button>
     </form>
 }
